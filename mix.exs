23cf803beafee8672f1c0c4141b56886ac007d07
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-04-08 16:00:50 +0300

    eh

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Sup.MixProject do
     [
       app: :sup,
       version: "0.1.0",
-      elixir: "~> 1.14-dev",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
